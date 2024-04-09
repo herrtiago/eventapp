2fb808b5199f9b8504ee132eef47644997f5c327
angieclemus <angielem11@gmail.com>
2024-04-09 16:53:27 -0500

    nav bar
    agregue home y el nav bar

---
+++ frontend_eventapp/lib/nav_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class BottomBarView extends StatefulWidget {
+  BottomBarView({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBarView> createState() => _BottomBarViewState();
+}
+
+class _BottomBarViewState extends State<BottomBarView> {
+  int currentIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  List<Widget> widgetOption = [
+    Placeholder(), //reemplazar Placeholder con pantallas reales
+    Placeholder(),
+    Placeholder(),
+    Placeholder(),
+    Placeholder(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onItemTapped,
+        selectedItemColor: Colors.black,
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Community',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Create Event',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: widgetOption[currentIndex],
+    );
+  }
+}
