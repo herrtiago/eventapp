0d812d989e9a27a0f1fac9815b730672361be8a2
angieclemus <angielem11@gmail.com>
2024-04-15 13:47:30 -0500

    .

--- frontend_eventapp/lib/login_page.dart
+++ frontend_eventapp/lib/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import '/sesion.dart';
+import '/sesion.dart'; 
 import 'package:google_fonts/google_fonts.dart';
 
 
