c7c0433e75ad26e43c3c0cbec5d0787d5b0d64ba
angieclemus <angielem11@gmail.com>
2024-04-15 14:05:24 -0500

    .

--- frontend_eventapp/test/widget_test.dart
+++ frontend_eventapp/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:frontend_eventapp/loginPage.dart';
+import 'package:frontend_eventapp/login_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
