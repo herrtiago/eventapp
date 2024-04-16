28dbfeafcc3bc207c2ea032b90ef290d3c2ef98f
angieclemus <angielem11@gmail.com>
2024-04-16 12:15:36 -0500

    arreglo diseño

--- frontend_eventapp/lib/login_page.dart
+++ frontend_eventapp/lib/login_page.dart
@@ -35,103 +35,130 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Login Page',
-          style: GoogleFonts.justAnotherHand(
-            textStyle: const TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 4, 61, 90),
-      ),
       body: Container(
         color: Color.fromARGB(255, 255, 255, 255),
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(80),
-                child: Image.asset(
-                  'images/logo.png',  // Path to the local image file
-                  height: 150,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.4, 
+                child: Container(
+                  padding: const EdgeInsets.all(50), 
+                  child: Image.asset(
+                    'images/logo.png',
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
-              const SizedBox(height: 50),
+              SizedBox(height: 20), 
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                padding: const EdgeInsets.symmetric(horizontal: 40),
                 child: TextField(
                   decoration: InputDecoration(
                     labelText: 'Usuario',
                     filled: true,
-                    fillColor: const Color.fromARGB(142, 218, 104, 38), // Color de fondo
-                    labelStyle: TextStyle(color: const Color.fromARGB(186, 255, 255, 255), fontSize: 30),
+                    fillColor: const Color.fromARGB(142, 218, 104, 38),
+                    labelStyle: TextStyle(
+                      color: const Color.fromARGB(186, 255, 255, 255),
+                      fontSize: 17,
+                      fontFamily: 'Karla',
+                    ),
+                    contentPadding: EdgeInsets.only(left: 20),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20), // Bordes curvos
-                      borderSide: BorderSide.none, // Eliminar los bordes
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
                     ),
                   ),
                   controller: _controllerUsername,
                 ),
               ),
+              SizedBox(height: 33),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                padding: const EdgeInsets.symmetric(horizontal: 40),
                 child: TextField(
                   decoration: InputDecoration(
                     labelText: 'Contraseña',
                     filled: true,
-                    fillColor: Color.fromARGB(142, 218, 104, 38), // Color de fondo
-                    labelStyle: TextStyle(color: const Color.fromARGB(186, 255, 255, 255), fontSize: 30),
+                    fillColor: Color.fromARGB(142, 218, 104, 38),
+                    labelStyle: TextStyle(
+                      color: const Color.fromARGB(186, 255, 255, 255),
+                      fontSize: 17,
+                      fontFamily: 'Karla',
+                    ),
+                    contentPadding: EdgeInsets.only(left: 20),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20), // Bordes curvos
-                      borderSide: BorderSide.none, // Eliminar los bordes
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
                     ),
                   ),
                   obscureText: true,
                   controller: _controllerPassword,
                 ),
               ),
-              const SizedBox(height: 20),
-              MouseRegion(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    final response = await http.post(
-                      Uri.parse('http://20.163.25.147:8000/login'),
-                      body: {
-                        'username': _controllerUsername.text,
-                        'password': _controllerPassword.text,
-                      },
-                    );
-                    if (response.statusCode == 200) {
-                      final SharedPreferences prefs = await _prefs;
-                      await prefs.setString('token', response.body);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Sesion()),
-                      );
-                    }
-                  },
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.resolveWith<Size>(
-                      (states) {
-                        if (states.contains(MaterialState.hovered)) {
-                          return const Size(200, 60); //hover
-                        }
-                        return const Size(180, 50); //normal
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 60),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: false,
+                      onChanged: (newValue) {
+                        //cambio del valor del checkbox
                       },
                     ),
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD96826)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
+                    Text(
+                      'Recordar contraseña',
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontFamily: 'Karla',
                       ),
                     ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () async {
+                  final response = await http.post(
+                    Uri.parse('http://20.163.25.147:8000/login'),
+                    body: {
+                      'username': _controllerUsername.text,
+                      'password': _controllerPassword.text,
+                    },
+                  );
+                  if (response.statusCode == 200) {
+                    final SharedPreferences prefs = await _prefs;
+                    await prefs.setString('token', response.body);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Sesion()),
+                    );
+                  }
+                },
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.resolveWith<Size>(
+                    (states) {
+                      if (states.contains(MaterialState.hovered)) {
+                        return const Size(200, 60); 
+                      }
+                      return const Size(180, 50); 
+                    },
                   ),
-                  child: const Text('Login',
-                      style: TextStyle(color: Colors.white, fontSize: 30)),
+                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD96826)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Iniciar Sesión',
+                  style: TextStyle(
+                    color: Color.fromARGB(186, 255, 255, 255),
+                    fontSize: 15,
+                    fontFamily: 'Karla',
+                  ),
                 ),
               ),
             ],
