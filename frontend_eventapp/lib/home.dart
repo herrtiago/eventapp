130b7a1d5559b8e22ae8043e1ed0b8d6fed47f38
angieclemus <angielem11@gmail.com>
2024-05-07 11:15:37 -0500

    Update home.dart

--- frontend_eventapp/lib/home.dart
+++ frontend_eventapp/lib/home.dart
@@ -30,27 +30,6 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
-        // Agrega el BottomNavigationBar aquí
-        // BottomNavigationBar(
-        //   items: const [
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.home),
-        //       label: 'Inicio',
-        //     ),
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.event),
-        //       label: 'Mis eventos',
-        //     ),
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.search),
-        //       label: 'Buscar',
-        //     ),
-        //   ],
-        //   // Controla la selección del ítem
-        //   currentIndex: 0,
-        //   selectedItemColor: Colors.black,
-        //   onTap: (index) {},
-        // ),
         BottomBarView(),
       ],
     );
